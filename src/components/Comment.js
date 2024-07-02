ab7221d52dad824173852548bcb532593842e1c5
jezinho <jez.johns@gmail.com>
2024-07-02 21:49:22 +0100

    retreat to mvp with no recursive commenting

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,19 +1,15 @@
 "use client";
 import { useState } from "react";
 import CommentForm from "./CommentForm"
-import ChildComment from "./ChildComment" 
 
-
-export default function Comment ({comment, userId, postId}) {
+export default function Comment ({comment}) {
   console.log("COMMENT: ", comment)
 
   return (
         <li key={"comment_" + comment.commentId} className="bg-glaucous text-mintcream border-russianviolet m-5 p-2">
 
           <div>{comment.comment}</div>
-              <p>{comment.author} 👍 <span className="">3</span> 👎</p>
-          <CommentForm userId={userId} postId={postId} parentId={comment.commentId}/>
-          <ChildComment parentId={comment.id} postId={postId}/>
+          <p>{comment.author} 👍 <span className="">3</span> 👎</p>
 
         </li>
   )
